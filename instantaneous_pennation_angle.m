89880bef1f55761e7b9e7446fb66a2354b766f35
rschuetze <85596564+rschuetze@users.noreply.github.com>
2023-03-24 18:26:56 -0400

    Add files via upload

---
+++ instantaneous_pennation_angle.m
@@ -0,0 +1,22 @@
+function instantaneous_pennation_angle (instantaneous_lm) 
+
+
+% male 9.4 female 8.7
+resting_pennation_angle [8.7, 9.4];
+%Female and Male
+
+resultant_lm = instantaneous_lm - global resting_LM;
+
+width = 0.47*resultant_lm;
+
+initial_vertical_reference_lm = global resting_LM * cos(resting_pennation_angle);
+%AB
+
+final_vertical_reference_lm = resultant_lm + vertical_reference_lm;
+%AC
+
+instantaneous_pa = arctan(width/final_vertical_reference_lm);
+
+return instantaneous_pa;
+
+end
