28f3608ae47f901a46249afba3ecf20d4c2b4faa
egilmour <78985611+egilmour@users.noreply.github.com>
2023-03-25 17:43:37 -0400

    Update force_length_tendon.m

--- force_length_tendon.m
+++ force_length_tendon.m
@@ -1,14 +1,14 @@
 function [normalize_tendon_tension] = force_length_tendon(instantaneous_lt)
-%%%% TASK 1
+
 % Input Parameters
 % instantaneous_lt: normalized length of tendon (series elastic element)
 % Output
 % normalized tension produced by tendon
 
        %functions for normalized force-length of the SE
-   if instantaneous_lt(i)<1
-       normalize_tendon_tension(i)=0;
+   if instantaneous_lt < 1
+       normalize_tendon_tension = 0;
    else
-       normalize_tendon_tension(i)=10*(instantaneous_lt(i)-1)+240*(instantaneous_lt(i)-1)^2;
+       normalize_tendon_tension = 10*(instantaneous_lt-1)+240*(instantaneous_lt-1)^2;
    end
 end
