48532d519da01c386ec986d38231e15bae2c4828
Zain-Ul-Abideen Altaf <77017146+zain-altaf@users.noreply.github.com>
2023-03-25 17:28:32 -0400

    Create force_length_parallel.m

---
+++ force_length_parallel.m
@@ -0,0 +1,18 @@
+function [normalize_PE_force] = force_length_parallel(lm)
+
+% Input Parameter
+% lm: normalized length of muscle (contractile element)
+
+% Output
+% normalized force produced by parallel elastic element
+
+%The objective here is to obtain normalized parallel elastic values to return to
+%any function that requires the parallel elastic element
+if lm < 1
+    normalized_pe = 0;
+else
+    normalized_pe = (3 * ((lm - 1)^2)) / (0.6 + lm - 1);
+end
+
+normalize_PE_force = normalized_pe;
+end
