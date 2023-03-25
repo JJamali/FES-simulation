d7bbc0bdb9fc92963ace02998950b616a3683a9b
rschuetze <85596564+rschuetze@users.noreply.github.com>
2023-03-25 17:51:45 -0400

    Update instantaneous_pennation_angle.m

--- instantaneous_pennation_angle.m
+++ instantaneous_pennation_angle.m
@@ -1,12 +1,10 @@
 function [instantaneous_pa] = instantaneous_pennation_angle (instantaneous_lm) 
 
-    instantaneous_lm = 1.01
-    resting_lm = 1
-    resting_pa = 9.4
-    % male 9.4
+    global resting_pa
 
     numerator = (sind(resting_pa)/0.47 + cosd(resting_pa))*sind(atand(0.47));
 
     instantaneous_pa = 180 - atand(0.47) - (180 - asind(numerator/instantaneous_lm))
+    
     
 end
