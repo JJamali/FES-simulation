3e4984b2c31bb1b534d716deb7fd63125d82c504
Zain-Ul-Abideen Altaf <77017146+zain-altaf@users.noreply.github.com>
2023-03-25 21:28:03 -0400

    Update get_muscle_velocity.m

--- get_muscle_velocity.m
+++ get_muscle_velocity.m
@@ -3,6 +3,6 @@ function [vm] = get_muscle_velocity(activation, norm_lm, norm_lt, pennation_angl
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
-vm = finverse((force_length_tendon(norm_lt)/cos(pennation_angle)-force_length_parallel(norm_lm))/(activation*force_length_muscle(norm_lm)));
+vm = finverse((force_length_tendon(norm_lt)/cosd(pennation_angle)-force_length_parallel(norm_lm))/(activation*force_length_muscle(norm_lm)));
 
 end
